8da24bd2db332a87d39b3cabd6c0caaab0cc32d9
Jeremy Taylor <goldplatedmarketer@gmail.com>
2017-02-12 15:21:35 -0800

    commit

---
+++ object_2.js
@@ -0,0 +1,13 @@
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function() {
+      
+      return this.firstName + " " + this.lastName;
+    }
+  };
+  return person;
+}
