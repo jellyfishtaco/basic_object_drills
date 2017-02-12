60838620922ff372d60e5a900caf2b188ac5f873
Jeremy Taylor <goldplatedmarketer@gmail.com>
2017-02-12 15:29:02 -0800

    four commit

---
+++ object.js
@@ -0,0 +1,14 @@
+function updateObject(obj) {
+  
+  return Object.assign({foo:'foo', bar:'bar', bizz:'bizz', bang:'bang'}, obj)
+    
+   
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+
+}
+  
+  
+  
