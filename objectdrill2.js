51b1a356a9fe5612e90baba29fc7a32c17e71064
Jeremy Taylor <goldplatedmarketer@gmail.com>
2017-02-16 18:28:53 -0800

    object drills

---
+++ objectdrill2.js
@@ -0,0 +1,16 @@
+
+
+function enrollInSummerSchool(students) {
+  
+  
+  
+  for (i = 0; i < students.length; i++) {
+    
+    students[i].status = 'In Summer school';
+  }
+  
+  return students;
+  
+
+};
+  
